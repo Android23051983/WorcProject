964d5ce89295f3d7d9eb2e558309558735cb97cf
Andrei <ikar2008@hotmail.com>
2022-05-07 15:23:33 +0300

    Add project files.

---
+++ WorkProject/main.cpp
@@ -0,0 +1,413 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(float arr[], const int n);
+void FillRand(long long int arr[], const int n);
+void FillRand(char arr[], const int n);
+
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(float arr[], const int n);
+void Print(long long int arr[], const int n);
+void Print(char arr[], const int n);
+
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+void Sort(float arr[], const int n);
+void Sort(long long int arr[], const int n);
+void Sort(char arr[], const int n);
+
+int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+float Sum(float arr[], const int n);
+long long int Sum(long long int arr[], const int n);
+int Sum(char arr[], const int n);
+
+double Avg(int arr[], const int n);	//Avg (Avarage - Среднее арифметическое)
+double Avg(double arr[], const int n);	//Avg (Avarage - Среднее арифметическое)
+float Avg(float arr[], const int n);	//Avg (Avarage - Среднее арифметическое)
+double Avg(long long int arr[], const int n);	//Avg (Avarage - Среднее арифметическое)
+double Avg(char arr[], const int n);	//Avg (Avarage - Среднее арифметическое)
+
+int minValueIn(int arr[], const int n);
+double minValueIn(double arr[], const int n);
+float minValueIn(float arr[], const int n);
+long long int minValueIn(long long int arr[], const int n);
+char minValueIn(char arr[], const int n);
+
+int maxValueIn(int arr[], const int n);
+double maxValueIn(double arr[], const int n);
+float maxValueIn(float arr[], const int n);
+long long int maxValueIn(long long int arr[], const int n);
+char maxValueIn(char arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(arr, n) << endl;
+	Sort(arr, n);
+	Print(arr, n);
+	cout << "----------------------------------------------------------------------------" << endl;
+
+	const int m = 8;
+	float brr[m];
+	FillRand(brr, m);
+	Print(brr, m);
+	cout << "Сумма элементов массива: " << Sum(brr, m) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(brr, m) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(brr, m) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(brr, m) << endl;
+	Sort(brr, m);
+	Print(brr, m);
+	cout << "----------------------------------------------------------------------------" << endl;
+
+	const int l = 4;
+	char crr[l];
+	FillRand(crr, l);
+	Print(crr, l);
+	cout << "Сумма элементов массива: " << Sum(crr, l) << endl;
+	cout << "Среднее-арифметическое элементов массива: " << Avg(crr, l) << endl;
+	cout << "Минимальное значение в массиве: " << minValueIn(crr, l) << endl;
+	cout << "Максимальное значение в массиве: " << maxValueIn(crr, l) << endl;
+	Sort(crr, l);
+	Print(crr, l);
+}
+void FillRand(int arr[], const int n)
+{
+	//Заполняем случайными чослами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void FillRand(double arr[], const int n)
+{
+	//Заполняем случайными чослами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
+	}
+}
+void FillRand(float arr[], const int n)
+{
+	//Заполняем случайными чослами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
+	}
+}
+void FillRand(long long int arr[], const int n)
+{
+	//Заполняем случайными чослами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+
+	}
+}
+void FillRand(char arr[], const int n)
+{
+	//Заполняем случайными чослами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(double arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(float arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(long long int arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(char arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << int(arr[i]) << "\t";
+	}
+	cout << endl;
+}
+
+void Sort(int arr[], const int n)
+{
+	//Сортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(double arr[], const int n)
+{
+	//Сортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				double buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(float arr[], const int n)
+{
+	//Сортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				float buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(long long int arr[], const int n)
+{
+	//Сортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				long long int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(char arr[], const int n)
+{
+	//Сортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				char buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
+int Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Sum(double arr[], const int n)
+{
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+float Sum(float arr[], const int n)
+{
+	float sum = 0.0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+long long int Sum(long long int arr[], const int n)
+{
+	long long int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+int Sum(char arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+double Avg(int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+double Avg(double arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+float Avg(float arr[], const int n)
+{
+	return (float)Sum(arr, n) / n;
+}
+double Avg(long long int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+double Avg(char arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
+
+int minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+double minValueIn(double arr[], const int n)
+{
+	double min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+float minValueIn(float arr[], const int n)
+{
+	float min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+long long int minValueIn(long long int arr[], const int n)
+{
+	long long int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+char minValueIn(char arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+double maxValueIn(double arr[], const int n)
+{
+	double max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+float maxValueIn(float arr[], const int n)
+{
+	float max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+long long int maxValueIn(long long int arr[], const int n)
+{
+	long long int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+char maxValueIn(char arr[], const int n)
+{
+	char max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
